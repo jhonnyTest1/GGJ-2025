ae923bf06ed38ca6f4e60a5b74d232154cfed75e
Drafj <juanpabloc2000@gmail.com>
2025-01-24 23:46:55 -0500

    IAttack interface

---
+++ Assets/Scripts/EnemyAttacks/RangerAttack.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class RangerAttack : MonoBehaviour, IAttack
+{
+    public void Attack(int damage)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
